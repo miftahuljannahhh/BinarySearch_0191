f0161a3d2fb5de6cb25c98c108154bd618c99777
miftahuljannahhh <mj5416386@gmail.com>
2025-05-07 15:14:38 +0700

    commit 3

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -24,6 +24,17 @@ void input()
     
 }
 
+cout << "\n======================================\n";
+cout << "          Masukkan Elemen Array        \n";
+cout << "=======================================\n";
+for (int i = 0; i < nPanjang; i++)
+{
+    cout << "Data ke-" << (i + 1) << " = ";
+    cin >> element[i];
+}
+
+
+
 
 int main()
 {
